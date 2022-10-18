706cc29445447258804f7f7de86275ed5a81b788
jeongminlee <chopin.vii@gmail.com>
2022-10-18 17:48:24 +0900

    revert

--- src/routes/post/[id]/+page.ts
+++ src/routes/post/[id]/+page.ts
@@ -5,14 +5,27 @@ export const load: PageLoad = async ({ params, fetch }) => {
   let post: Post | null = null
   let posts: Post[] = []
   try {
+<<<<<<< HEAD
     let result = await fetch(`/api/post/${params.id}`, {
       method: 'GET'
+=======
+    let result = await fetch(`${fetchUrl}/post/${params.id}`, {
+      method: 'GET',
+      mode: 'no-cors'
+>>>>>>> parent of 297768e... Revert "cors"
     })
     if (result.ok) {
       post = await result.json()
     }
 
+<<<<<<< HEAD
     result = await fetch(`/api/post/category?cat=${post?.category}`)
+=======
+    result = await fetch(`${fetchUrl}/post/category?cat=${post?.category}`, {
+      method: 'GET',
+      mode: 'no-cors'
+    })
+>>>>>>> parent of 297768e... Revert "cors"
     if (result.ok) {
       posts = await result.json()
     }
