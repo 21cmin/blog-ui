9263f03dfeaa88ea6eaa5e7b946f555eb239ed20
jeongminlee <chopin.vii@gmail.com>
2022-10-18 17:54:41 +0900

    cors

--- src/routes/write/+page.ts
+++ src/routes/write/+page.ts
@@ -8,8 +8,14 @@ export const load: PageLoad = async ({ url, fetch }) => {
     try {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       const result = await fetch(`/api/post/${query}`, {
         method: 'GET'
+=======
+      const result = await fetch(`${fetchUrl}/post/${query}`, {
+        method: 'GET',
+        mode: 'no-cors'
+>>>>>>> parent of 297768e... Revert "cors"
 =======
       const result = await fetch(`${fetchUrl}/post/${query}`, {
         method: 'GET',
